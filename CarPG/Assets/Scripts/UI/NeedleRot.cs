5443bd64e21655a73fdb52f7cb744df1dd3a606a
mxo9722 <mxo9722@rit.edu>
2019-11-16 15:19:07 -0500

    minor changes

--- CarPG/Assets/Scripts/UI/NeedleRot.cs
+++ CarPG/Assets/Scripts/UI/NeedleRot.cs
@@ -1,28 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NeedleRot : MonoBehaviour
+namespace UnityStandardAssets.Vehicles.Car
 {
-
-    GameObject car;
-    Rigidbody rb;
-    RectTransform rTransform;
-
-    float startRot = 127.35f;
-
-    private void Start()
+    public class NeedleRot : MonoBehaviour
     {
-        car = GameObject.FindWithTag("Player");
-        rb = car.GetComponent<Rigidbody>();
-        rTransform = GetComponent<RectTransform>();
 
-        //startRot = transform.rotation.z;
-    }
+        GameObject car;
+        CarController cController;
+        RectTransform rTransform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        rTransform.rotation = Quaternion.Euler(0,0, startRot - (rb.velocity.magnitude * 1.975f));
+        float startRot = 127.35f;
+
+        private void Start()
+        {
+            car = GameObject.FindWithTag("Player");
+            cController = car.GetComponent<CarController>();
+            rTransform = GetComponent<RectTransform>();
+
+            //startRot = transform.rotation.z;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rTransform.rotation = Quaternion.Euler(0, 0, startRot - (cController.CurrentSpeed * 1.975f));
+        }
     }
 }
