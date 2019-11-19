b6db646a56090b334ebf655d3cfc0114ea40c713
mike benton <mxb3183@rit.edu>
2019-11-19 03:06:59 -0500

    Camera sees through annoying walls
    It's not optimized but hey it works

---
+++ CarPG/Assets/Scripts/CameraTransparencyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransparencyScript : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject car;
+
+    private List<MeshRenderer> hiddenObjects = new List<MeshRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hiddenObjects.Count; i++)
+        {
+            hiddenObjects[i].enabled = true;
+            hiddenObjects[i].gameObject.layer = 0;
+        }
+        hiddenObjects.Clear();
+
+        RaycastHit hit;
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.4F, 0.1F, 0));
+
+        while (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall")
+        {
+                hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                hit.transform.gameObject.layer = 2;
+                hiddenObjects.Add(hit.transform.gameObject.GetComponent<MeshRenderer>());
+        }
+
+        ray = camera.ViewportPointToRay(new Vector3(0.6F, 0.1F, 0));
+        while (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall")
+        {
+            hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            hit.transform.gameObject.layer = 2;
+            hiddenObjects.Add(hit.transform.gameObject.GetComponent<MeshRenderer>());
+        }
+    }
+}
