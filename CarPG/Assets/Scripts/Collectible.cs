46e85aa0ef7204a2b03bfc4ec80f696d1976fbc6
Stephen Hoffman <sch4240@g.rit.edu>
2020-02-04 13:32:42 -0500

    Collectible System
    Collectible system is now implemented

---
+++ CarPG/Assets/Scripts/Collectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        gameObject.SendMessage("Collect", SendMessageOptions.DontRequireReceiver);
+        Destroy(gameObject);
+    }
+}
