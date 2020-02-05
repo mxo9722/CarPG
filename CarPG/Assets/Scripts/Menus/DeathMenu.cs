d218c88ec8d87c61d29fe02a1e6c7b43df5aa149
Stephen Hoffman <sch4240@g.rit.edu>
2020-02-04 22:31:17 -0500

    Death screen working
    When you die the game now tells you with a handy screen

---
+++ CarPG/Assets/Scripts/Menus/DeathMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        //if (EditorUtility.DisplayDialog("Return To Main Menu", "Are you sure you'd like to return to the main menu? Your progress will be lost.", "Return to Main Menu", "Continue Playing"))
+        //{
+        SceneManager.LoadScene("Title", LoadSceneMode.Single);
+        //}
+    }
+
+    public void Exit()
+    {
+        //if (EditorUtility.DisplayDialog("Exit","Are you sure you'd like to quit? Your progress will be lost.","Exit","Continue Playing"))
+        //{
+        Application.Quit();
+        //}
+    }
+
+    public void Replay()
+    {
+        //Replay code will go here
+    }
+
+    /*public void LoadOptions()
+    {
+        SceneManager.LoadSceneAsync(AssetDatabase.GetAssetPath(optionsMenu),LoadSceneMode.Additive);
+    }*/
+}
