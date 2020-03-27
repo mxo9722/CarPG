162fe4e7a49f851c4494384ad0ec2ed4882330eb
mike benton <mxb3183@rit.edu>
2020-03-27 19:51:37 -0400

    Damage Hitsplats
    We gotta standardize the damage threshold a bit also you hit things several times.  I only have it enabled for enemies right now because the terrain keeps damaging itself.

---
+++ CarPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DamageTextController.Initialize(); //This thing is basically all static methods so we just initialize it here because why not
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
