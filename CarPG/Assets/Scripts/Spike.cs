f221d228765f96cd2ed2e98e266cc654d5c0782a
Stephen Hoffman <sch4240@g.rit.edu>
2020-03-30 19:31:23 -0400

    Spikes inprogress
    Spikes are not quite at smooth functionality but underlying work is there

---
+++ CarPG/Assets/Scripts/Spike.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public float spikeCooldown;
+    bool offCD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spikeCooldown = 2f;
+        offCD = true;
+    }
+
+    void Update()
+    {
+        if(offCD == false)
+        {
+            spikeCooldown -= Time.deltaTime; 
+            if (spikeCooldown < 0)
+            {
+                offCD = true;
+            }
+        }
+        
+    }
+
+    public void trigger()
+    {
+        if(offCD == true)
+        {
+            float objY = gameObject.transform.position.y;
+            if(objY < 0.5)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, objY += 0.1f, gameObject.transform.position.z);
+            }
+            else
+            {
+                spikeCooldown = 2f;
+                offCD = false;
+            }
+            
+        }
+        
+    }
+}
---
+++ CarPG/Assets/Scripts/SpikeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeManager : MonoBehaviour
+{
+    Spike[] spikes;
+    GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spikes = GetComponentsInChildren<Spike>(true);
+        car = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, car.transform.position);
+        if(dist < 10)
+        {
+            for(int i=0; i < spikes.Length; i++)
+            {
+                spikes[i].trigger();
+                if (spikes[i].spikeCooldown > 0)
+                {
+                    float objY = spikes[i].transform.position.y;
+                    spikes[i].transform.position = new Vector3(spikes[i].transform.position.x, objY -= 0.2f, spikes[i].transform.position.z);
+                }
+            }
+        }
+    }
+}
