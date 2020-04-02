c51f649905850f482caa39cc0abb7893bb291459
mxo9722 <mxo9722@rit.edu>
2020-04-02 13:05:29 -0400

    Boss base made and added

---
+++ CarPG/Assets/Scripts/Enemy/Boss/BossAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+
+    enum BossStates
+    {
+        BeamAttack,
+        BeamCool,
+        RapidFireAttack,
+        RapidCool,
+        CreateHealers,
+        HealerCool
+    }
+
+    private BossStates curState;
+
+    private int lives = 3;
+
+    public GameObject car;
+
+    void Start()
+    {
+        car = GameObject.FindGameObjectWithTag("Car");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BeamAttack()
+    {
+
+    }
+
+    void BeamCool()
+    {
+
+    }
+
+    void RapidFireAttack()
+    {
+
+    }
+
+    void RapidCool()
+    {
+
+    }
+
+    void CreateHealers()
+    {
+
+    }
+
+    void HealerCool()
+    {
+
+    }
+}
